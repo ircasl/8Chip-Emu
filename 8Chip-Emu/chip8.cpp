70bd080c0b7fef72576df362c36af78e2ae217df
Kirian <irchad.98@gmail.com>
2019-12-16 13:52:50 +0000

    0xFX33

--- 8Chip-Emu/chip8.cpp
+++ 8Chip-Emu/chip8.cpp
@@ -176,6 +176,23 @@ void chip8::emulateCycle()
 				PC += 2;
 			}
 			break;
+		case 0xF000:
+			switch (OPCode & 0x00FF)
+			{
+				case 0x0033: // FX33: Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1,
+							 // and the least significant digit at I plus 2. 
+							 //(In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I,
+							 //the tens digit at location I+1, and the ones digit at location I+2.)
+					Memory[I] = V[(OPCode & 0x0F00) >> 8] / 100; //Get the most significant digit
+					Memory[I + 1] = (V[(OPCode & 0x0F00) >> 8] / 10) % 10;   //Get the middle digit
+					Memory[I + 2] = (V[(OPCode & 0x0F00) >> 8] % 100) % 10;   //Get the least significant digit
+					PC += 2;
+					break;
+				default:
+					printf("Unknown opcode: 0x%X\n", OPCode);
+					break;
+			}
+			break;
 		default:
 			printf("Unknown opcode: 0x%X\n", OPCode);
 	}
